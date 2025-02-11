d4aa8e6c796e042af21457797de2b1ea63d6f624
MizukiOyama <154662763+MizukiOyama@users.noreply.github.com.>
2025-02-11 15:10:31 +0900

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -55,9 +55,9 @@ module.exports = {
       new HtmlWebpackPlugin({ template: "./src/service.html", filename: "service.html", chunks: ["main"] }),
       new HtmlWebpackPlugin({ template: "./src/contact.html", filename: "contact.html", chunks: ["main"] }),
       new HtmlWebpackPlugin({ template: "./src/policy.html", filename: "policy.html", chunks: ["main"] }),
-      new HtmlWebpackPlugin({ template: "./src/blog/categories.html", filename: "categories.html", chunks: ["main"] }),
       new HtmlWebpackPlugin({ template: "./src/header.html", filename: "header.html", chunks: ["main"] }),
       new HtmlWebpackPlugin({ template: "./src/footer.html", filename: "footer.html", chunks: ["main"] }),
+      new HtmlWebpackPlugin({ template: "./src/blog/categories.html", filename: "categories.html", chunks: ["blog"] }),
       new MiniCssExtractPlugin({ filename: "styles/main.css" }),
       new CopyWebpackPlugin({
          patterns: [
