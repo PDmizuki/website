f1508a30957847b0bfd323c331fe58272dd9f9fc
m.oyama <154662763+PDmizuki@users.noreply.github.com>
2025-04-01 05:53:30 +0000

    mobile tab form

--- src/js/form.js
+++ src/js/form.js
@@ -1,25 +1,17 @@
-// グローバル変数 'submitted' を宣言
-let submitted = false;
-
 document.addEventListener("DOMContentLoaded", function () {
   const inputs = document.querySelectorAll(".input-text");
 
-  inputs.forEach(input => {
-    // 初期チェック
-    toggleLabel(input);
-
-    // 入力イベントに応じてクラスを切り替え
-    input.addEventListener("input", function () {
-      toggleLabel(input);
-    });
-
-    function toggleLabel(input) {
-      if (input.value !== "") {
-        input.classList.add("not-empty");
-      } else {
-        input.classList.remove("not-empty");
-      }
+  function toggleLabel(input) {
+    if (input.value.trim() !== "") {
+      input.classList.add("not-empty");
+    } else {
+      input.classList.remove("not-empty");
     }
+  }
+
+  inputs.forEach(input => {
+    toggleLabel(input);
+    input.addEventListener("input", () => toggleLabel(input));
   });
 
 
