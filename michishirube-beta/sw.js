7089dac2fe53cb05c7aa6d2f1b3fad72f95d0ef2
m.oyama <154662763+mizukioyama@users.noreply.github.com>
2026-09-09 09:30:41 +0900

    Add Michishirube beta service worker

---
+++ michishirube-beta/sw.js
@@ -0,0 +1,16 @@
+const CACHE='michishirube-beta-v06';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting()});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const url=new URL(event.request.url);
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  if(url.origin===location.origin){
+    event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return res})));
+  }
+});
