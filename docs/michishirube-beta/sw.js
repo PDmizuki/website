85996f7fececfe4e9ceef2aabfee9f1e40dc05fe
m.oyama <154662763+mizukioyama@users.noreply.github.com>
2026-09-09 09:44:09 +0900

    Bump Michishirube PWA cache to v0.7

--- docs/michishirube-beta/sw.js
+++ docs/michishirube-beta/sw.js
@@ -1,4 +1,4 @@
-const CACHE='michishirube-beta-v06';
+const CACHE='michishirube-beta-v07';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
